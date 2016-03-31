6eaaca9b26f6d3cc3cffdd036fa7de28f89ae33b
Sami Liedes <sami.liedes@iki.fi>
2016-03-31 22:04:55 +0300

    Add GPLv2+

--- pawnsonly.cpp
+++ pawnsonly.cpp
@@ -1,3 +1,19 @@
+// Copyright (C) 2016  Sami Liedes
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 #include "MemTranspositionTable.hpp"
 #include "binom.hpp"
 #include <algorithm>
@@ -24,7 +40,7 @@ static constexpr bool DEBUG = true;
 // static constexpr int N = 7;
 // static constexpr int VERBOSE_DEPTH = 3;
 // static constexpr int PARALLEL_DEPTH = 10;
-// static constexpr int ALPHABETA_MIN_DEPTH = 0;
+// static constexpr int CUT_MIN_DEPTH = 0;
 // static constexpr int PARALLEL_MIN_DEPTH = 0;
 // static constexpr size_t TP_TABLE_SIZE = 671088637; // 2.5 gigabytes
 
